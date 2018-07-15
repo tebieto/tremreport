f9e2b5b3741222f54a75d2129f111d8516b59188
GerrardE <ezeugwagerrard@gmail.com>
2018-07-15 11:28:57 +0100

    3rd commit styles

--- resources/views/inc/links.blade.php
+++ resources/views/inc/links.blade.php
@@ -3,15 +3,18 @@
     <li role="presentation"><a href="/weekly">WEEKLY</a></li>
     <li role="presentation"><a href="/g12">G12</a></li>
     <li role="presentation"><a href="#">EVENTS</a></li>
+    <li role="presentation"><a href="#">DOWNLOADS</a></li>
     <li class="dropdown" role="presentation">
         <a class="dropdown-toggle" data-toggle="dropdown">SETTINGS<span class="caret"></span></a>
         <ul class="dropdown-menu">
-            <li><a href="#">Create Branch</a></li>
-            <li><a href="#">Create Event</a></li>
-            <li><a href="#">Create Pastor</a></li>
-            <li><a href="#">Create Preacher</a></li>
-            <li><a href="#">Create User</a></li>         
-            <li><a href="#">Create G12</a></li>              
+            <li class="dropdown-header">CREATE</li>
+            <li class="divider"></li>
+            <li><a href="#">BRANCH</a></li>
+            <li><a href="#">G12</a></li>
+            <li><a href="#">EVENT</a></li>
+            <li><a href="#">PASTOR</a></li>
+            <li><a href="#">PREACHER</a></li>
+            <li><a href="#">USER</a></li>                       
         </ul>
     </li>
 </ul>
