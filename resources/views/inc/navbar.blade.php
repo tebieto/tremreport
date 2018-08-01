211e90ebe714358d93ad7928abfaf2b08a9c7689
Terry Ebieto <tebieto@gmail.com>
2018-08-01 20:39:34 +0100

    committing new changes

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -16,14 +16,14 @@
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="https://www.trem.org">HOME</a></li>
-                
+                @if (auth::check())
 					<li id="logout">
 						<form  method="post" action="/logout">
 						{{ csrf_field() }}
 						<button>LOGOUT</button>
 						</form>
 					</li>
-				
+				@endif
 				</ul>
             </div><!--/.nav-collapse -->
     </div>
